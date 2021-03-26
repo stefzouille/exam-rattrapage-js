31bb8b771c445556f3705287d59d37d336d20dd3
stefzouille <contact.lokcy@gmail.com>
2021-03-26 17:09:11 +0100

    add

--- threeWayLoop.js
+++ threeWayLoop.js
@@ -13,16 +13,20 @@ const tab = ['Alice', 'Bob', 'Charlie', 'Dan', 'Eve']
 
 
 for (j = tab.length-1; j>=0; j--) { 
+  
   console.log(`${tab[j]}, length : ${tab[j].length} index : ${[j]}`) //affiche le tableau en index et la longueur de chacun
 }
 
 
 
-// for (const elem of tab) {                               //elements du tab assigné a une variable
-//   console.log(`${elem}, length: ${elem.length}`)        // affiche les elements et la longueur de chacun
-// }
+for (const elem of tab) {                               //elements du tab assigné a une variable
+  elem.split('').reverse('')
+  console.log(`${elem}, length: ${elem.length} index : ${[j]}`)        // affiche les elements et la longueur de chacun
+}
 
-// tab.forEach((elem) => {                                 //methode forEach passé en fonction
-//   let ar1 = []
-// console.log(`${elem}, length: ${elem.length}`)          // affiche les elements et la longueur de chacun
-// })
+tab.forEach((elem) => {                                 //methode forEach passé en fonction
+  
+console.log(`${elem}, length: ${elem.length} index : ${[j]}`)          // affiche les elements et la longueur de chacun
+})
+
+// dsl  reussi a return que la boucle for
