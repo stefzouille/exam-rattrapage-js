0bcab411769192a637133eb40bc4d83a39bb9a9e
stefzouille <contact.lokcy@gmail.com>
2021-03-26 17:04:09 +0100

    add

--- convertEuroToDollar.js
+++ convertEuroToDollar.js
@@ -1,6 +1,11 @@
 // Ecrire une fonction convertEuroToDollar qui fait la conversion
 //  d'une somme exprimée en Euros, en Dollars. 1 euro est égal à 1.18 dollars
 
+
+const RATIO =  1.18
 const convertEuroToDollar = (sum) => {
-  return 
+  return  RATIO * sum
 }
+
+ 
+console.log(`${convertEuroToDollar(10)} Dollars.`)
